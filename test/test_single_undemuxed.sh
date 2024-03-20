683600419ee5edeb8aeda6d66835fa7be04bc767
mykle hoban <mhoban@hawaii.edu>
2024-03-20 11:48:35 -1000

    moved some test data around

--- test/test_single_undemuxed.sh
+++ test/test_single_undemuxed.sh
@@ -6,7 +6,7 @@ cd single_test_undemux
 ../../eDNAFlow.nf \
   --single \
   --ignore-blast-env \
-  --reads '../fastq/test_30000reads.fastq' \
+  --reads '../fastq/single_undemux/test_30000reads.fastq' \
   --barcode '../se_bc_*' \
   --blast-db ../blast/single_undemuxed \
   --collapse-taxonomy
